e8d3741d0726341d675d7757fa2cfb48be6fd999
Gabriel Calogero <gabicalogero@gmail.com>
2021-05-27 23:53:09 -0300

    agrega carpeta components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
